29c80afd4f1ef68e19568cbaebf163198a44c747
Paramveer <paramveermarwahafc@gmail.com>
2025-08-18 14:03:34 +0200

    feat: logging in dev mode; app server setup

---
+++ server.js
@@ -0,0 +1,13 @@
+import { app } from "./app.js";
+import morgan from "morgan";
+import "./dbConfig/db.js";
+
+if (process.env.NODE_ENV !== "production") {
+  app.use(morgan("dev"));
+}
+
+const port = process.env.PORT || 5100;
+
+app.listen(port, () => {
+  console.log(`Server is listening on PORT ${port} in ${process.env.NODE_ENV}`);
+});
