2e3e57306a1f49967ee9bf7b19b99877140f272d
Paramveer <paramveermarwahafc@gmail.com>
2025-08-23 13:07:35 +0200

    feat(users): add /users/current-user and admin list/make-admin endpoints (protected)

---
+++ controllers/userController.js
@@ -0,0 +1,25 @@
+import { StatusCodes } from "http-status-codes";
+import User from "../models/User.js";
+import { asyncWrapper } from "../utils/asyncWrapper.js";
+
+export const getCurrentUser = asyncWrapper(async function (req, res) {
+  const user = await User.findById(req.user.id);
+  res.status(StatusCodes.OK).json({ status: "success", user });
+});
+
+// restricted route
+export const getAllUsers = asyncWrapper(async function (req, res) {
+  const users = await User.find();
+  res
+    .status(StatusCodes.OK)
+    .json({ status: "success", results: users.length, data: { users } });
+});
+
+export const makeAdmin = asyncWrapper(async function (req, res) {
+  const user = await User.findById(req.params.id);
+  if (!user) throw new NotFoundError("User not found");
+  user.role = "admin";
+  await user.save();
+
+  res.status(StatusCodes.OK).json({ status: "success", data: { user } });
+});
