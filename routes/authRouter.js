81d95920921ca124260fea080a722ee5616dcfe9
Paramveer <paramveermarwahafc@gmail.com>
2025-08-23 13:09:58 +0200

    feat: grouped authentication endpoints; validate signup login

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,3 +1,10 @@
 import express from "express";
+import { login, logout, signup } from "../controllers/authController.js";
+import { validate } from "../middlewares/validate.js";
+import { loginSchema, registerSchema } from "../validation/userValidation.js";
 
 export const authrouter = express.Router();
+
+authrouter.post("/signup", validate(registerSchema), signup);
+authrouter.post("/login", validate(loginSchema), login);
+authrouter.get("/logout", logout);
